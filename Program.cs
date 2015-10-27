dd6d4e2179cd56e0c25ecf552c99d2f98677f4ec
shiale14 <beshemel@gmail.com>
2015-10-27 14:18:12 +0100

    Added comments/changed TryToBuy method. Now the information about amount of cookies that were bought by each customer is available on the bottom of the console text.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace TheCookieBakery
 {
 	class Program
 	{
-		static readonly TheBakery Bakery = new TheBakery(20);
+
+		static readonly TheBakery Bakery = new TheBakery(21);
 		static readonly Stopwatch Delay = new Stopwatch();
 
 		public static void Main(string[] args)
 		{
+			// starts the timer
 			Delay.Start();
+
+			// initialization of local variables
 			var fredTheCustomer = new Customer("Fred");
 			var tedTheCustomer = new Customer("Ted");
 			var gregTheCustomer = new Customer("Greg");
@@ -22,32 +26,55 @@ namespace TheCookieBakery
 			threads[2] = new Thread(delegate () { TryToBuy(tedTheCustomer); });
 			threads[3] = new Thread(delegate () { TryToBuy(gregTheCustomer); });
 
+			// starts the threads
 			for (var i = 0; i < 4; i++)
 			{
 				threads[i].Start();
 			}
 
+			// waits for the threads are done
 			for (var i = 0; i < 4; i++) {
 				threads[i].Join();
 			}
 
+			// waits for the user to close the console
 			Pause();
 
 		}
+
+		/// <summary>
+		/// To wait for the user to close the program
+		/// </summary>
 		private static void Pause()
 		{
-			Console.ForegroundColor = ConsoleColor.DarkRed;
+//			Console.ForegroundColor = ConsoleColor.DarkRed; // Uncomment to change the color 
+															//of the final prompt
 			Console.WriteLine("\nPress any key to close the bakery...");
 			Console.ReadKey(true);
 		}
 
+		/// <summary>
+		/// Each "customer"-thread has this method as main. The method waits in 950 milliseconds
+		/// and calls the SellCookieTo method from TheBakery object if the instance of TheBakery object
+		/// still has cookies or has to produce more cookies (check in BakeryIsClosed method). 
+		/// To ensure next attempt the method restarts the timer. 
+		/// If there are no cookies and the bakery isn't producing more the methods writes to the console
+		/// the amount of the cookies the customer has bought.
+		/// </summary>
+		/// <param name="customer">Concrete customer who tries to buy a cookie</param>
 		private static void TryToBuy(Customer customer)
 		{
+			var count = 0;
 			while (true)
 			{
 				if (Delay.ElapsedMilliseconds <= 950) continue;
-				if (Bakery.BakeryIsClosed()) return;
-				Bakery.SellCookieTo(customer);
+				if (Bakery.BakeryIsClosed())
+				{
+					Console.WriteLine("\t\t " + customer.GetName() + 
+						" has bought " + count + " cookies.");
+					return;
+				}
+				if(Bakery.SellCookieTo(customer)) count++;
 				Delay.Restart();
 			}
 		}
