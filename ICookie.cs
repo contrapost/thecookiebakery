9919cf1905bfd9ac32b69d7a30a29500ae1f726b
shiale14 <beshemel@gmail.com>
2015-10-30 19:40:49 +0100

    comment

--- ICookie.cs
+++ ICookie.cs
@@ -3,6 +3,8 @@
 // NOTE: ICookie interface, abstract class CookieDecorator and class UsualCookie that inherit from
 //		 ICookie, classes AlmondDecorator, ChocolateDecorator and RaisinDecorator are parts of the
 //		 decorator design pattern thar is used by TheBakery class to produce different cookies.
+//		 To add a new cookie type make a new decorator class, change CookieTypes enum and ChooseCookie
+//		 method in TheBakery class.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
