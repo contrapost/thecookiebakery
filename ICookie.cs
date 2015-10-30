357db6795761f89d2990405aa120f08f07e4824a
shiale14 <beshemel@gmail.com>
2015-10-30 01:02:09 +0100

    Comments.

--- ICookie.cs
+++ ICookie.cs
@@ -1,4 +1,12 @@
-﻿namespace TheCookieBakery
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// NOTE: ICookie interface, abstract class CookieDecorator and class UsualCookie that inherit from
+//		 ICookie, classes AlmondDecorator, ChocolateDecorator and RaisinDecorator are parts of the
+//		 decorator design pattern thar is used by TheBakery class to produce different cookies.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace TheCookieBakery
 {
 	interface ICookie
 	{
