3f342f368d4c6f6b7f6d52cfd6a4064209c4ae92
shiale14 <beshemel@gmail.com>
2015-10-30 01:00:38 +0100

    Pause method is changed. Comments.

--- BakeryController.cs
+++ BakeryController.cs
@@ -8,7 +8,8 @@ namespace TheCookieBakery
 	public class BakeryController
 	{
 		private readonly TheBakery _bakery;
-		private readonly List<Customer> _customers; 
+		private readonly List<Customer> _customers;
+		 
 		public BakeryController(TheBakery bakery, List<Customer> customers)
 		{
 			_bakery = bakery;
@@ -20,35 +21,44 @@ namespace TheCookieBakery
 			// Can be used to change the title of the console and the text color.
 			Gui.MakeGui("The Cookie Bakery", ConsoleColor.White);
 
+			// This part of the code deals with multithreading.
+			#region Multithreading
+			// initialization of the araay of threads
 			var threads = new Thread[_customers.Count + 1];
 
+			// initialization of the bakery-thread
 			threads[0] = new Thread(_bakery.MakeCookie);
 
+			// initialization of customer-threads
             for (var i = 1; i < threads.Length; i++)
 			{
 				var index = i;
 				threads[i] = new Thread(delegate () { _customers.ElementAt(index - 1).TryToBuy(_bakery); });
 			}
 
+			// start of the threads
 			foreach (var t in threads)
 			{
 				t.Start();
 			}
 
+			// wait untill of threads are done
 			foreach (var t in threads) {
 				t.Join();
 			}
+			#endregion
 
-			// waits for the user to close the console
-			Pause();
+			// Waits for the user to close the console.
+			Pause(ConsoleColor.White);
 		}
+
 		/// <summary>
-		/// To wait for the user to close the program
+		/// Waits for the user to close the console. The color of the prompt can be changed.
 		/// </summary>
-		private static void Pause()
+		/// <param name="color">ConsoleColor of the final prompt</param>
+		private static void Pause(ConsoleColor color)
 		{
-			//			Console.ForegroundColor = ConsoleColor.DarkRed; // Uncomment to change the color 
-			//of the final prompt
+			Console.ForegroundColor = color; 
 			Console.WriteLine("\nPress any key to close the bakery...");
 			Console.ReadKey(true);
 		}
