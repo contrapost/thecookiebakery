161bea668c1d4f2ada56fce8a8a3871e3de160b3
shiale14 <beshemel@gmail.com>
2015-10-30 01:01:48 +0100

    Comments.

--- Gui.cs
+++ Gui.cs
@@ -4,6 +4,11 @@ namespace TheCookieBakery
 {
 	class Gui
 	{
+		/// <summary>
+		/// Set the title of the console and the color of the text.
+		/// </summary>
+		/// <param name="consoleTitle"></param>
+		/// <param name="color"></param>
 		public static void MakeGui(string consoleTitle, ConsoleColor color)
 		{
 			Console.Title = consoleTitle;
